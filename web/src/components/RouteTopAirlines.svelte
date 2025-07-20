c4e9b6b57730f5b07703d1a438ab47889dd749f1
Tom Carman <tom.carman@gmail.com>
2025-07-20 19:46:43 +0100

    feat: add airline logos to RouteTopAirlines.svelte

--- web/src/components/RouteTopAirlines.svelte
+++ web/src/components/RouteTopAirlines.svelte
@@ -57,7 +57,9 @@
 
     {#each data as airline, index}
     <li class="list-row">
-        <div class="text-4xl font-thin opacity-30 tabular-nums">{String(index + 1).padStart(2, '0')}</div>
+         <div>
+            <img src="https://doj0yisjozhv1.cloudfront.net/square-logos/{airline.airline_icao}.png" width="40" height="40" alt="{airline.airline_icao}">
+         </div>
         <div class="list-col-grow">
         <div class="font-medium">{airline.airline_name}</div>
         <div class="text-xs uppercase font-semibold opacity-60">{airline.airline_icao} / {airline.airline_iata}</div>
